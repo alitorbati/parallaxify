c07b5e7a9baf407ed15674a0e9b9977775c83817
Ali Torbati <ali.torbati@gmail.com>
2015-09-02 00:16:28 -0700

    Protect $. Improve percentage return value logic.

--- parallaxify.js
+++ parallaxify.js
@@ -1,28 +1,34 @@
-$.fn.parallaxify = function() {
-  return this.each(function() {
-    var $element = $(this);
+// Created by Ali Torbati
+// ali.torbati@gmail.com
+// 2015
 
-    function percentageSeen () {
-      var viewportHeight = $(window).height(),
-          scrollTop = $(window).scrollTop(),
-          elementOffsetTop = $element.offset().top,
-          elementHeight = $element.height();
+(function ($) {
 
-      if (elementOffsetTop > (scrollTop + viewportHeight)) {
-        return 0;
-      } else if ((elementOffsetTop + elementHeight) < scrollTop) {
-        return 100;
-      } else {
-        var distance = (scrollTop + viewportHeight) - elementOffsetTop;
+  $.fn.parallaxify = function() {
+
+    return this.each(function() {
+      var $element = $(this);
+
+      function percentageSeen () {
+        var viewportHeight = $(window).height(),
+            winScrollTop = $(window).scrollTop(),
+            elementOffsetTop = $element.offset().top,
+            elementHeight = $element.height();
+
+        var distance = (winScrollTop + viewportHeight) - elementOffsetTop;
         var percentage = distance / ((viewportHeight + elementHeight) / 100);
-        return percentage;
+
+        if (percentage < 0) return 0;
+        else if (percentage > 100) return 100;
+        else return percentage;
       }
-    }
 
-    $element.css({ 'background-position-y' : percentageSeen()+'%' });
-
-    $(window).on('scroll', function() {
       $element.css({ 'background-position-y' : percentageSeen()+'%' });
+
+      $(window).on('scroll', function() {
+        $element.css({ 'background-position-y' : percentageSeen()+'%' });
+      });
     });
-  });
-};
+  };
+
+}(jQuery));
