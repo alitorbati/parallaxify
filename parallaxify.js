db72ae89af743638e1cc647be7adeecae42bf71a
Ali Torbati <ali.torbati@gmail.com>
2016-08-17 20:00:34 -0700

    version 2.2 - efficiency

--- parallaxify.js
+++ parallaxify.js
@@ -1,21 +1,23 @@
 // Created by Ali Torbati
 // ali.torbati@gmail.com
 // 2016
-// Version 2.1
+// Version 2.2
 
 'use strict'
 
-let parallaxify = function(elements) {
-  elements = document.querySelectorAll(elements);
+let parallaxify = function(selector) {
+  let elements = document.querySelectorAll(selector)
+  let lastPercent = 0
+
   for (let i = 0; i < elements.length; i++) {
-    let element = elements[i],
-        elementOffsetTop = element.offsetTop,
-        elementHeight = element.offsetHeight;
+    let element = elements[i]
+    let elementOffsetTop = element.offsetTop
+    let elementHeight = element.offsetHeight
+    let viewportHeight = 0
+    let winScrollTop = 0
 
     function percentSeen () {
-      let viewportHeight = window.innerHeight,
-          winScrollTop = window.scrollY;
-
+      winScrollTop = window.scrollY
       let distance = (winScrollTop + viewportHeight) - elementOffsetTop
       let percentage = distance / ((viewportHeight + elementHeight) / 100)
 
@@ -24,12 +26,30 @@ let parallaxify = function(elements) {
       else return percentage
     }
 
-    // initialize
-    element.style.backgroundPositionY = percentSeen()+'%'
+    let updateWindowCalcs = function () {
+      viewportHeight = window.innerHeight
+    }
 
-    // update
+    // update background position only if it has changed
+    let updateBgPosition = function () {
+      if (lastPercent != percentSeen()) {
+        element.style.backgroundPositionY = percentSeen()+'%'
+      }
+      lastPercent = percentSeen()
+    }
+
+    // initialize
+    updateWindowCalcs()
+    updateBgPosition()
+
+    // update background position on scroll
     window.addEventListener('scroll', function(){
-      element.style.backgroundPositionY = percentSeen()+'%'
+      updateBgPosition()
+    })
+
+    // update calculations on resize
+    window.addEventListener('resize', function(){
+      updateWindowCalcs()
     })
   }
 }
