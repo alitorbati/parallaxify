213b672560e48dd9bedaa0483640568f2587ed8c
Ali Torbati <ali.torbati@gmail.com>
2016-08-17 18:11:32 -0700

    ES6 and multiple instances of a class.

--- parallaxify.js
+++ parallaxify.js
@@ -1,30 +1,35 @@
 // Created by Ali Torbati
 // ali.torbati@gmail.com
 // 2016
-// Version 2.0
+// Version 2.1
 
-var parallaxify = function(element) {
-  var element = document.querySelectorAll(element)[0],
-      elementOffsetTop = element.offsetTop,
-      elementHeight = element.offsetHeight;
+'use strict'
 
-  function percentageSeen () {
-    var viewportHeight = window.innerHeight,
-        winScrollTop = window.scrollY;
+let parallaxify = function(elements) {
+  elements = document.querySelectorAll(elements);
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i],
+        elementOffsetTop = element.offsetTop,
+        elementHeight = element.offsetHeight;
 
-    var distance = (winScrollTop + viewportHeight) - elementOffsetTop;
-    var percentage = distance / ((viewportHeight + elementHeight) / 100);
+    function percentSeen () {
+      let viewportHeight = window.innerHeight,
+          winScrollTop = window.scrollY;
 
-    if (percentage < 0) return 0;
-    else if (percentage > 100) return 100;
-    else return percentage;
+      let distance = (winScrollTop + viewportHeight) - elementOffsetTop
+      let percentage = distance / ((viewportHeight + elementHeight) / 100)
+
+      if (percentage < 0) return 0
+      else if (percentage > 100) return 100
+      else return percentage
+    }
+
+    // initialize
+    element.style.backgroundPositionY = percentSeen()+'%'
+
+    // update
+    window.addEventListener('scroll', function(){
+      element.style.backgroundPositionY = percentSeen()+'%'
+    })
   }
-
-  // initial
-  element.style.backgroundPositionY = percentageSeen()+'%'
-
-  // update
-  window.addEventListener('scroll', function(){
-    element.style.backgroundPositionY = percentageSeen()+'%'
-  });
-};
+}
