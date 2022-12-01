266574dcab5c50700363e093aa135e62b80d5af0
Ali Torbati <ali.torbati@gmail.com>
2022-12-01 00:03:37 -0800

    refactor, update README

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # parallaxify
-JavaScript snippet to easily add a subtle parallax effect to background-images. No longer requires jQuery.
 
-## javascript
-Just use `parallaxify('.image_container');` to activate the effect. Of course, you can use it on an element with any class.
+A zero-dependency, super-lightweight JavaScript snippet to add a subtle parallax effect to background-images
 
-## css
-Use this css for your element:
+![Demo](demo.gif)
+
+## HTML
+
+```html
+<script src="parallaxify.js"></script>
 ```
-.image_container {
-  background-image: url('./images/example.jpg');
+
+## JavaScript
+
+```js
+document.addEventListener("DOMContentLoaded", (event) => {
+  parallaxify(".some-class");
+});
+```
+
+## CSS
+
+```css
+.some-class {
+  height: 130px; /* any value smaller than the image height */
+  background-image: url("some-image.jpg");
   background-size: cover;
   background-position-x: 50%;
   background-position-y: 0%;
--- parallaxify.js
+++ parallaxify.js
@@ -1,55 +1,23 @@
-// Created by Ali Torbati
-// ali.torbati@gmail.com
-// 2016
-// Version 2.2
+"use strict";
 
-'use strict'
+function parallaxify(selector) {
+  const elements = document.querySelectorAll(selector);
 
-let parallaxify = function(selector) {
-  let elements = document.querySelectorAll(selector)
-  let lastPercent = 0
+  function getPercentSeen(el) {
+    let distance = window.scrollY + window.innerHeight - el.offsetTop;
+    let percentage = distance / ((window.innerHeight + el.offsetHeight) / 100);
+    return Math.min(Math.max(percentage, 0), 100); // clamp between 0 and 100
+  }
 
   for (let i = 0; i < elements.length; i++) {
-    let element = elements[i]
-    let elementOffsetTop = element.offsetTop
-    let elementHeight = element.offsetHeight
-    let viewportHeight = 0
-    let winScrollTop = 0
-
-    function percentSeen () {
-      winScrollTop = window.scrollY
-      let distance = (winScrollTop + viewportHeight) - elementOffsetTop
-      let percentage = distance / ((viewportHeight + elementHeight) / 100)
-
-      if (percentage < 0) return 0
-      else if (percentage > 100) return 100
-      else return percentage
-    }
-
-    let updateWindowCalcs = function () {
-      viewportHeight = window.innerHeight
-    }
-
-    // update background position only if it has changed
-    let updateBgPosition = function () {
-      if (lastPercent != percentSeen()) {
-        element.style.backgroundPositionY = percentSeen()+'%'
-      }
-      lastPercent = percentSeen()
-    }
-
-    // initialize
-    updateWindowCalcs()
-    updateBgPosition()
-
-    // update background position on scroll
-    window.addEventListener('scroll', function(){
-      updateBgPosition()
-    })
-
-    // update calculations on resize
-    window.addEventListener('resize', function(){
-      updateWindowCalcs()
-    })
+    const el = elements[i];
+    el.style.backgroundPositionY = `${getPercentSeen(el)}%`;
+    window.addEventListener("scroll", function () {
+      el.style.backgroundPositionY = `${getPercentSeen(el)}%`;
+    });
   }
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  parallaxify(".header-image");
+});
